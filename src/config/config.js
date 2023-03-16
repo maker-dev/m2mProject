db7474d76f5d9539df370886fefb29f6c30e8667
maker-dev <anasaizen9@gmail.com>
2023-03-16 17:47:34 +0200

    secure firebase sdk

--- src/config/config.js
+++ src/config/config.js
@@ -4,12 +4,12 @@ import {getAuth} from 'firebase/auth';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ8GhM_s3_I9Nibo-wwcOIi-fo7eqrQ28",
-    authDomain: "m2mproject-f2537.firebaseapp.com",
-    projectId: "m2mproject-f2537",
-    storageBucket: "m2mproject-f2537.appspot.com",
-    messagingSenderId: "189082892698",
-    appId: "1:189082892698:web:deeb50c45bd2ded4b62223"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
